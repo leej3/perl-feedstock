0fb0648229695cf46721d14afb787ef86c56c286
Ray Donnelly <mingw.android@gmail.com>
2017-06-26 15:44:38 +0100

    Add cross-compilation support (WIP)
    I had to disable tests here. I *think* one test is broken or
    else the wrapping-at-900 chars feature in perlbug is broken.
    
    Also pass -Dsysroot to prevent Configure looking in /usr

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=$(pwd)
-
 # world-writable files are not allowed
-chmod -R o-w $SRC_DIR
+chmod -R o-w "${SRC_DIR}"
 
 # Give install_name_tool enough room to work its magic
-if [ `uname -s` == "Darwin" ]; then
-    export LDFLAGS="${LDFLAGS} -Wl,-headerpad_max_install_names"
+if [[ $(uname) == Darwin ]]; then
+  LDFLAGS=${LDFLAGS}" -Wl,-headerpad_max_install_names"
 fi
 
-sh Configure -Dusethreads -Duserelocatableinc -Dprefix=$PREFIX -de -Aldflags="$LDFLAGS"
+if [[ -n ${HOST} ]] && [[ -n ${CC} ]]; then
+  SYSROOT=$(dirname $(dirname ${CC}))/$(${CC} -dumpmachine)/sysroot
+else
+  SYSROOT=/usr
+fi
+
+if [[ -n "${CC}" ]]; then
+  CC_OPT="-Dcc=${CC}"
+fi
+
+# -Dsysroot prevents Configure rummaging around in /usr and
+# linking to system libraries (like GDBM, which is GPL). An
+# alternative is to pass -Dusecrosscompile but that prevents
+# all Configure/run checks which we also do not want.
+./Configure -de -Dprefix=${PREFIX}                \
+                ${CC_OPT}                         \
+                -Dcccdlflags="-fPIC"              \
+                -Dlddlflags="-shared ${LDFLAGS}"  \
+                -Dldflags="${LDFLAGS}"            \
+                -Dusethreads                      \
+                -Duserelocatableinc               \
+                -Dsysroot=${SYSROOT}
 make
 
 # change permissions again after building
-chmod -R o-w $SRC_DIR
+chmod -R o-w "${SRC_DIR}"
 
-make test
+# Seems we hit:
+# lib/perlbug .................................................... # Failed test 21 - [perl \#128020] long body lines are wrapped: maxlen 1157 at ../lib/perlbug.t line 154
+# FAILED at test 21
+# https://rt.perl.org/Public/Bug/Display.html?id=128020
+# make test
 make install
